7ed3d9ab0f5a12792027406cbc1e9e403672414f
boogeh <wagner.amit@gmail.com>
2018-10-11 13:19:06 +0300

    added images

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -3,12 +3,32 @@ var gIdx = 1
 var gProjects;
 
 
+function getProjects() {
+    return gProjects
+}
+
 function createProjects() {
- gProjects = [createProject('PacMan', 'PacMan in a 2d array', 'The first really lousy pacman in the world!', 'url', ['game', 'pacman'])]
+    gProjects = [
+        createProject(
+            'PacMan',
+            'PacMan in a 2d array',
+            'The first really lousy pacman in the world!',
+            'url',
+            ['game', 'pacman'],
+            'https://via.placeholder.com/400x300'),
+        createProject(
+            'Minesweeper',
+            'Minesweeper homemade!',
+            'My first sprint test. Made a minesweeper!',
+            'https://boogeh.github.io/minesweeper/',
+            ['game', 'minesweeper'],
+            '/img/portfolio/minesweeper.jpg')
+
+    ]
 }
 
 
-function createProject(name, title, desc, url, labels) {
+function createProject(name, title, desc, url, labels, img) {
     return {
         id: gIdx++,
         name: name,
@@ -16,7 +36,15 @@ function createProject(name, title, desc, url, labels) {
         desc: desc,
         url: url,
         publishedAt: Date.now(),
-        labels: labels
+        labels: labels,
+        img: img
     }
 
+}
+
+function getProjectById(projId) {
+    var proj = gProjects.find(function (project) {
+        return project.id === projId
+    })
+    return proj
 }
--- js/main.js
+++ js/main.js
@@ -3,21 +3,42 @@ console.log('Starting up');
 
 function init() {
     createProjects()
+    renderPortfolio()
 }
 
-function renderModal(projectClicked) {
-    console.log(projectClicked)
+function renderPortfolio() {
+  var projects = getProjects()
+  var strHtmls = projects.map(function(project) {
+    return `<div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" onclick ="renderModal(${project.id})" href="#portfolioModal1">
+      <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+        </div>
+      </div>
+      <img class="img-fluid" src="${project.img}" alt="">
+    </a>
+    <div class="portfolio-caption">
+      <h4>${project.name}</h4>
+      <p class="text-muted">${project.title}</p>
+    </div>
+    </div>`
+  })
+  console.log(strHtmls.join())
+  $('.portfolio-render').html(strHtmls.join(''))
+}
+
+function renderModal(projId) {
+    var currProject = getProjectById(projId)
     var strHtml = `
-    <h2>Testing</h2>
-    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+    <h2>${currProject.name}</h2>
+    <p class="item-intro text-muted">${currProject.title}</p>
     <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
-    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis
-      dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
-      maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+    <p>${currProject.desc}</p>
     <ul class="list-inline">
-      <li>Date: January 2017</li>
-      <li>Client: Threads</li>
-      <li>Category: Illustration</li>
+      <li>Date: October 2018</li>
+      <li>gitHub link: ${currProject.url}</li>
+      <li>labels: ${currProject.labels}</li>
     </ul>
     <button class="btn btn-primary" data-dismiss="modal" type="button">
         <i class="fa fa-times"></i>
